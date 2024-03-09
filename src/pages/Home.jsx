16ed10bc8ef945e7235cb0b3c186be30242b78ab
torn <zhabkapepe5@gmail.com>
2024-03-09 13:50:24 +0500

    returned accidentally deleted home changes with thunkAsyncFunction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,13 @@ import {
   setCurrentPage,
   setFilters,
 } from '../redux/slices/filterSlice';
+import { fetchPizzas } from '../redux/slices/PizzaSlice';
 
 import { Pagination } from '../components/Pagination';
 import { Categories } from '../components/Categories';
 import { Sort, sortList } from '../components/Sort';
 import { PizzaBlock, Skeleton } from '../components/PizzaBlock';
 import { SearchContext } from '../App';
-
-import axios from 'axios';
 
 function Home() {
   const navigate = useNavigate();
@@ -26,9 +25,9 @@ function Home() {
   const { categoryId, sort, currentPage } = useSelector(
     (state) => state.filter
   );
+  const { items, status } = useSelector((state) => state.pizza);
 
   const { searchValue } = React.useContext(SearchContext);
-  const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   const onChangeCategory = (id) => {
@@ -39,34 +38,23 @@ function Home() {
     dispatch(setCurrentPage(number));
   };
 
-  const fetchPizzas = () => {
-    setIsLoading(true);
-
+  const getPizzas = () => {
     const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sort.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
 
-    axios
-      .get(
-        `https://65c5bde8e5b94dfca2e039c5.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-      )
-      .then((res) => {
-        if (Array.isArray(res.data)) {
-          setItems(res.data);
-        } else {
-          setItems([]);
-        }
-        setIsLoading(false);
+    dispatch(
+      fetchPizzas({
+        order,
+        sortBy,
+        category,
+        search,
+        currentPage,
       })
-      .catch((error) => {
-        if (error.response && error.response.status === 404) {
-          setItems([]);
-          setIsLoading(false);
-        } else {
-          console.error('Произошла ошибка при загрузке данных', error);
-        }
-      });
+    );
+
+    window.scrollTo(0, 0);
   };
 
   React.useEffect(() => {
@@ -83,12 +71,7 @@ function Home() {
   }, []);
 
   React.useEffect(() => {
-    window.scrollTo(0, 0);
-
-    if (!isSearch.current) {
-      fetchPizzas();
-    }
-    isSearch.current = false;
+    getPizzas();
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
   React.useEffect(() => {
@@ -117,7 +100,19 @@ function Home() {
           <Sort />
         </div>
         <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+        {status === 'error' ? (
+          <div className="content__error-info">
+            <h2>Произошла ошибка 😕</h2>{' '}
+            <p>
+              К сожалению, не удалось загрузить пиццу. Попробуйте повторить
+              попытку позже.
+            </p>
+          </div>
+        ) : (
+          <div className="content__items">
+            {status === 'loading' ? skeletons : pizzas}
+          </div>
+        )}
         <Pagination currentPage={currentPage} onChangePage={onChangePage} />
       </div>
     </>
